f6d72e35abc61d5d4e9ba896e501e2306789e5c7
Jollen Chen <jollen@moko365.com>
2012-10-25 09:45:09 +0800

    Modify: use javascript module pattern

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
-    var canvas = document.getElementById("draw-background"),
-        ctx = canvas.getContext("2d");
+var game = (function() {
 
-        canvas.width = 240;
-        canvas.height = 320;
+    function drawBall() {
+        var canvas = document.getElementById("draw-background"),
+                ctx = canvas.getContext("2d");
 
-        ctx.beginPath();
-        ctx.arc(50, 100, 50, 0 * Math.PI, 2 * Math.PI, false);    
-        ctx.closePath();   
-        ctx.fill();
+                canvas.width = 240;
+                canvas.height = 320;
+
+                ctx.beginPath();
+                ctx.arc(50, 100, 50, 0 * Math.PI, 2 * Math.PI, false);    
+                ctx.closePath();   
+                ctx.fill();
+    }
+
+}) ();
