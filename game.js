78d746f39c002a2d614c237fb3dca55adc80ed99
Jollen Chen <jollen@moko365.com>
2012-10-25 09:41:39 +0800

    Modify: migrate javascript code to game.js

---
+++ game.js
@@ -0,0 +1,10 @@
+    var canvas = document.getElementById("draw-background"),
+        ctx = canvas.getContext("2d");
+
+        canvas.width = 240;
+        canvas.height = 320;
+
+        ctx.beginPath();
+        ctx.arc(50, 100, 50, 0 * Math.PI, 2 * Math.PI, false);    
+        ctx.closePath();   
+        ctx.fill();
